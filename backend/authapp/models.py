7df25e69b7d49ad7817fa41396832bfdc7ef44b2
FelipeWai <97367113+FelipeWai@users.noreply.github.com>
2024-03-06 00:20:04 -0300

    Recomeço

--- backend/authapp/models.py
+++ backend/authapp/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Users(models.Model):
     username = models.CharField(max_length=100)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     password = models.CharField(max_length=200)
