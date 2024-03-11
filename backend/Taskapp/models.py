eb455e27ab63f18195d6f0888694eb7c522a03a8
FelipeWai <97367113+FelipeWai@users.noreply.github.com>
2024-03-11 10:48:04 -0300

    Added tasks table

--- backend/Taskapp/models.py
+++ backend/Taskapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Tasks(models.Model):
+    title = models.CharField(max_length=20)
+    description = models.TextField(max_length=600)
+    status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.title}"
--- backend/Taskapp/tests.py
+++ backend/Taskapp/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
+from .models import Tasks
+
 # Create your tests here.
+class TasksTest(TestCase):
+    def setUp(self):
+        Tasks.objects.create(title="t1", description="TESTE 1", status="concluded")
+        Tasks.objects.create(title="t2", description="TESTE 2", status="progress")
+
+    def test_db_working(self):
+        self.assertEqual(Tasks.objects.count(), 2)
